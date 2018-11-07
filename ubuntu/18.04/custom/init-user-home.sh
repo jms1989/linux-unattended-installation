619046b37e74a28de5844176afb4f125bcc562f3
Michael SanAngelo <msanangelo@gmail.com>
2018-11-06 18:47:50 -0600

    change repo domain for dotfiles

--- ubuntu/18.04/custom/init-user-home.sh
+++ ubuntu/18.04/custom/init-user-home.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Clone dotfiles repository from GitHub"
-git clone https://github.com/jms1989/dotfiles.git ~/.dotfiles
+git clone https://gitlab.com/jms1989/dotfiles.git ~/.dotfiles
 bash ~/.dotfiles/install.sh
 echo "done."
 rm -f $0
