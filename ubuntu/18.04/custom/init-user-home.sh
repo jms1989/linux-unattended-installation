a81733a8d1caaba9ca227b5d012f2044ddb3c430
Michael SanAngelo <msanangelo@gmail.com>
2018-11-06 22:13:03 -0600

    suppress sudo message upon login

--- ubuntu/18.04/custom/init-user-home.sh
+++ ubuntu/18.04/custom/init-user-home.sh
@@ -2,6 +2,7 @@
 echo "Clone dotfiles repository from GitHub"
 git clone https://gitlab.com/jms1989/dotfiles.git ~/.dotfiles
 bash ~/.dotfiles/install.sh
+touch ~/.sudo_as_admin_successful
 echo "done."
 rm -f $0
 exit
